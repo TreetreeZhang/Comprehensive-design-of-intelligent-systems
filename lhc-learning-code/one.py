07760dbc9c7714d0e829bf23a2e9324adec4e45c
lhc <2502494553@qq.com>
2024-10-10 08:54:24 +0800

    add a viewer while robot  collapse

--- lhc-learning-code/one.py
+++ lhc-learning-code/one.py
@@ -1,9 +1,22 @@
+#!/usr/bin/env python3
+#!coding=utf-8
+
+"""
+Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+
+NVIDIA CORPORATION and its licensors retain all intellectual property
+and proprietary rights in and to this software, related documentation
+and any modifications thereto. Any use, reproduction, disclosure or
+distribution of this software and related documentation without an express
+license agreement from NVIDIA CORPORATION is strictly prohibited.
+"""
+
 from isaacgym import gymapi
 from isaacgym import gymutil
 gym = gymapi.acquire_gym()
 
 # parse arguments
-args = gymutil.parse_arguments(description="Asset and Environment Information")
+args = gymutil.parse_arguments(description="FR5 Asset and Environment Information")
 
 # get default set of parameters
 sim_params = gymapi.SimParams()
@@ -17,7 +30,7 @@ sim = gym.create_sim(args.compute_device_id, args.graphics_device_id, args.physi
 
 # configure the ground plane
 plane_params = gymapi.PlaneParams()
-plane_params.normal = gymapi.Vec3(0, 0, 1) # z-up!
+plane_params.normal = gymapi.Vec3(0, 1, 0) # z-up!
 plane_params.distance = 0
 plane_params.static_friction = 1
 plane_params.dynamic_friction = 1
@@ -36,15 +49,30 @@ asset_file = "urdf/fr5_robot_descripiton/robots/fr5_robot.urdf"
 asset = gym.load_asset(sim, asset_root, asset_file)
 
 spacing = 2.0
-lower = gymapi.Vec3(-spacing, 0.0, -spacing)
-upper = gymapi.Vec3(spacing, spacing, spacing)
-
-env = gym.create_env(sim, lower, upper, 1)
+env_lower = gymapi.Vec3(-spacing, 0.0, -spacing)
+env_upper = gymapi.Vec3(spacing, spacing, spacing)
 pose = gymapi.Transform()
 pose.p = gymapi.Vec3(0.0, 1.0, 0.0)
 pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
 
-actor_handle = gym.create_actor(env, asset, pose, "MyActor", 0, 1)
+# cache useful handles
+envs = []
+actor_handles = []
+num_envs = 25
+
+print("Creating %d environments" % num_envs)
+for i in range(num_envs):
+    # create env
+    env = gym.create_env(sim, env_lower, env_upper, 5)
+    envs.append(env)
+
+    # add actor
+    actor_handle = gym.create_actor(env, asset, pose, "actor", i, 1)
+    actor_handles.append(actor_handle)
+
+# env = gym.create_env(sim, env_lower, env_upper, 1)
+
+# actor_handle = gym.create_actor(env, asset, pose, "MyActor", 0, 1)
 
 actor_count = gym.get_actor_count(env)
 print("%d actors total" % actor_count)
@@ -54,5 +82,21 @@ for i in range(actor_count):
     actor_handle = gym.get_actor_handle(env, i)
     # print_actor_info(gym, env, actor_handle)
 
+cam_props = gymapi.CameraProperties()
+viewer = gym.create_viewer(sim, cam_props)
+
+while not gym.query_viewer_has_closed(viewer):
+
+    # step the physics
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+
+    # update the viewer
+    gym.step_graphics(sim)
+    gym.draw_viewer(viewer, sim, True)
+
+    # Wait for dt to elapse in real time.
+    # This synchronizes the physics simulation with the rendering rate.
+    gym.sync_frame_time(sim)
 # Cleanup the simulator
-gym.destroy_sim(sim)
+# gym.destroy_sim(sim)
