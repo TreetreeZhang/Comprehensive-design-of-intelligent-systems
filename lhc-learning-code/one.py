ac99ab03fa2325ed0cd19846da55c6ce800030b5
LiHaochuan123 <2502494553@qq.com>
2024-10-11 20:28:09 +0800

    successful insert robut which is not collapsed

--- lhc-learning-code/one.py
+++ lhc-learning-code/one.py
@@ -11,15 +11,32 @@ distribution of this software and related documentation without an express
 license agreement from NVIDIA CORPORATION is strictly prohibited.
 """
 
+import math
 from isaacgym import gymapi
 from isaacgym import gymutil
+
+# initialize gym
 gym = gymapi.acquire_gym()
 
-# parse arguments
-args = gymutil.parse_arguments(description="FR5 Asset and Environment Information")
+# Parse arguments
+args = gymutil.parse_arguments(description="first-one")
 
-# get default set of parameters
+# configure sim
 sim_params = gymapi.SimParams()
+sim_params.dt = 1.0 / 60.0
+sim_params.substeps = 2
+if args.physics_engine == gymapi.SIM_FLEX:
+    sim_params.flex.solver_type = 5
+    sim_params.flex.num_outer_iterations = 4
+    sim_params.flex.num_inner_iterations = 15
+    sim_params.flex.relaxation = 0.75
+    sim_params.flex.warm_start = 0.8
+elif args.physics_engine == gymapi.SIM_PHYSX:
+    sim_params.physx.solver_type = 1
+    sim_params.physx.num_position_iterations = 4
+    sim_params.physx.num_velocity_iterations = 1
+    sim_params.physx.num_threads = args.num_threads
+    sim_params.physx.use_gpu = args.use_gpu
 
 sim_params.use_gpu_pipeline = False
 if args.use_gpu_pipeline:
@@ -27,47 +44,68 @@ if args.use_gpu_pipeline:
 
 # create sim with these parameters
 sim = gym.create_sim(args.compute_device_id, args.graphics_device_id, args.physics_engine, sim_params)
+if sim is None:
+    print("*** Failed to create sim")
+    quit()
 
-# configure the ground plane
+
+
+# add ground plane
 plane_params = gymapi.PlaneParams()
-plane_params.normal = gymapi.Vec3(0, 1, 0) # z-up!
-plane_params.distance = 0
-plane_params.static_friction = 1
-plane_params.dynamic_friction = 1
-plane_params.restitution = 0
-
-# create the ground plane
 gym.add_ground(sim, plane_params)
 
+# load robot asset
 asset_root = "../assets"
-# fr5 robot URDF model
+    # fr5 robot URDF model
 asset_file = "urdf/fr5_robot_descripiton/robots/fr5_robot.urdf"
-
-# Example
+    # Example
 # asset_file = "urdf/franka_description/robots/franka_panda.urdf"
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+asset_options.disable_gravity = True
+asset_options.flip_visual_attachments = True
+asset_options.armature = 0.01
+robot_asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
 
-asset = gym.load_asset(sim, asset_root, asset_file)
-
-spacing = 2.0
+# Set up the env grid
+num_envs = 36
+spacing = 1.0
 env_lower = gymapi.Vec3(-spacing, 0.0, -spacing)
 env_upper = gymapi.Vec3(spacing, spacing, spacing)
-pose = gymapi.Transform()
-pose.p = gymapi.Vec3(0.0, 1.0, 0.0)
-pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
 
 # cache useful handles
 envs = []
 actor_handles = []
-num_envs = 25
+
+# Attractor setup
+attractor_handles = []
+attractor_properties = gymapi.AttractorProperties()
+attractor_properties.stiffness = 5e5
+attractor_properties.damping = 5e3
+
+# Make attractor in all axes
+attractor_properties.axes = gymapi.AXIS_ALL
+pose = gymapi.Transform()
+pose.p = gymapi.Vec3(0, 0.0, 0.0)
+pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
+
+# Create helper geometry used for visualization
+# Create an wireframe axis
+axes_geom = gymutil.AxesGeometry(0.1)
+# Create an wireframe sphere
+sphere_rot = gymapi.Quat.from_euler_zyx(0.5 * math.pi, 0, 0)
+sphere_pose = gymapi.Transform(r=sphere_rot)
+sphere_geom = gymutil.WireframeSphereGeometry(0.03, 12, 12, sphere_pose, color=(1, 0, 0))
 
 print("Creating %d environments" % num_envs)
+num_per_row = int(math.sqrt(num_envs))
 for i in range(num_envs):
     # create env
-    env = gym.create_env(sim, env_lower, env_upper, 5)
+    env = gym.create_env(sim, env_lower, env_upper, num_per_row)
     envs.append(env)
 
     # add actor
-    actor_handle = gym.create_actor(env, asset, pose, "actor", i, 1)
+    actor_handle = gym.create_actor(env, robot_asset, pose, "actor", i, 1)
     actor_handles.append(actor_handle)
 
 # env = gym.create_env(sim, env_lower, env_upper, 1)
@@ -84,6 +122,9 @@ for i in range(actor_count):
 
 cam_props = gymapi.CameraProperties()
 viewer = gym.create_viewer(sim, cam_props)
+if viewer is None:
+    print("*** Failed to create viewer")
+    quit()
 
 while not gym.query_viewer_has_closed(viewer):
 
@@ -98,5 +139,7 @@ while not gym.query_viewer_has_closed(viewer):
     # Wait for dt to elapse in real time.
     # This synchronizes the physics simulation with the rendering rate.
     gym.sync_frame_time(sim)
+
+gym.destroy_viewer(viewer)
 # Cleanup the simulator
-# gym.destroy_sim(sim)
+gym.destroy_sim(sim)
