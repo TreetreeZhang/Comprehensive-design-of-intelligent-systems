d44af6912c7677ec930289732a47cc6582a66571
LiHaochuan123 <2502494553@qq.com>
2024-10-12 20:44:53 +0800

    commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# lhc_learning_IsaacGym
+### About this repository
+
+This repository contains files of robot with reinforcement learning.
+
+
+### Pre-request
+
+Download the Isaac Gym Preview 4 release from the Download the Isaac Gym Preview 4 release from the [website](https://developer.nvidia.com/isaac-gym), then follow the installation instructions in the documentation. 
+
+Then you should clone the repository [IsaacGymEnvs ](https://github.com/isaac-sim/IsaacGymEnvs) and install it.
+
+### Load robots
+
+To load the fr5-robot, run this line
+```bash
+python load_robot.py
+```
--- scripts/load_robot.py
+++ scripts/load_robot.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 #!coding=utf-8
 
-"""
-Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
-
-NVIDIA CORPORATION and its licensors retain all intellectual property
-and proprietary rights in and to this software, related documentation
-and any modifications thereto. Any use, reproduction, disclosure or
-distribution of this software and related documentation without an express
-license agreement from NVIDIA CORPORATION is strictly prohibited.
-"""
-
 import math
 from isaacgym import gymapi
 from isaacgym import gymutil
+
+
 
 # initialize gym
 gym = gymapi.acquire_gym()
 
 # Parse arguments
-args = gymutil.parse_arguments(description="first-one")
+args = gymutil.parse_arguments(description="Load Robot")
 
 # configure sim
 sim_params = gymapi.SimParams()
@@ -56,76 +48,49 @@ gym.add_ground(sim, plane_params)
 
 # load robot asset
 asset_root = "../assets"
-    # fr5 robot URDF model
-asset_file = "urdf/fr5_robot_descripiton/robots/fr5_robot.urdf"
-    # Example
-# asset_file = "urdf/franka_description/robots/franka_panda.urdf"
+# fr5 robot URDF model
+asset_file = "fr5_robot_descripiton/robots/fr5_robot.urdf"
+
 asset_options = gymapi.AssetOptions()
 asset_options.fix_base_link = True
 asset_options.disable_gravity = True
 asset_options.flip_visual_attachments = True
-asset_options.armature = 0.01
 robot_asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
+
+
 
 # Set up the env grid
 num_envs = 36
+num_per_row = int(math.sqrt(num_envs))
+print("Creating %d environments" % num_envs)
+
 spacing = 1.0
 env_lower = gymapi.Vec3(-spacing, 0.0, -spacing)
 env_upper = gymapi.Vec3(spacing, spacing, spacing)
 
-# cache useful handles
-envs = []
-actor_handles = []
-
-# Attractor setup
-attractor_handles = []
-attractor_properties = gymapi.AttractorProperties()
-attractor_properties.stiffness = 5e5
-attractor_properties.damping = 5e3
-
-# Make attractor in all axes
-attractor_properties.axes = gymapi.AXIS_ALL
 pose = gymapi.Transform()
 pose.p = gymapi.Vec3(0, 0.0, 0.0)
 pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
 
-# Create helper geometry used for visualization
-# Create an wireframe axis
-axes_geom = gymutil.AxesGeometry(0.1)
-# Create an wireframe sphere
-sphere_rot = gymapi.Quat.from_euler_zyx(0.5 * math.pi, 0, 0)
-sphere_pose = gymapi.Transform(r=sphere_rot)
-sphere_geom = gymutil.WireframeSphereGeometry(0.03, 12, 12, sphere_pose, color=(1, 0, 0))
-
-print("Creating %d environments" % num_envs)
-num_per_row = int(math.sqrt(num_envs))
 for i in range(num_envs):
     # create env
     env = gym.create_env(sim, env_lower, env_upper, num_per_row)
-    envs.append(env)
 
     # add actor
     actor_handle = gym.create_actor(env, robot_asset, pose, "actor", i, 1)
-    actor_handles.append(actor_handle)
 
-# env = gym.create_env(sim, env_lower, env_upper, 1)
 
-# actor_handle = gym.create_actor(env, asset, pose, "MyActor", 0, 1)
 
-actor_count = gym.get_actor_count(env)
-print("%d actors total" % actor_count)
-
-# Iterate through all actors for the environment
-for i in range(actor_count):
-    actor_handle = gym.get_actor_handle(env, i)
-    # print_actor_info(gym, env, actor_handle)
-
+# Create viewer
 cam_props = gymapi.CameraProperties()
 viewer = gym.create_viewer(sim, cam_props)
 if viewer is None:
     print("*** Failed to create viewer")
     quit()
 
+
+
+# Keep the viewer
 while not gym.query_viewer_has_closed(viewer):
 
     # step the physics
@@ -141,5 +106,6 @@ while not gym.query_viewer_has_closed(viewer):
     gym.sync_frame_time(sim)
 
 gym.destroy_viewer(viewer)
+
 # Cleanup the simulator
 gym.destroy_sim(sim)
